c5bec0f9642b9050bc1884b33b37c8b070b169b2
Aleksey Ropan <nap0rbl4@gmail.com>
2020-06-25 06:06:03 +0300

    Fixed compatible

--- setup.py
+++ setup.py
@@ -13,7 +13,10 @@ install_reqs = parse_requirements('requirements.txt', session='null')
 
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
+try:
+    reqs = [str(ir.req) for ir in install_reqs]
+except AttributeError:
+    reqs = [str(ir.requirement) for ir in install_reqs]
 
 
 with open('README.md') as f:
